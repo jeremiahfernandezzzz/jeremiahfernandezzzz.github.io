b84ef68215d06de2ec862b74302d78fbfbfeca8d
jeremiahfernandezzzz <jeremiahfernandezzzz@gmail.com>
2017-04-17 19:07:54 -0700

    Update mainobf.js

--- script/mainobf.js
+++ script/mainobf.js
@@ -1,1 +1,241 @@
-var _0x6e21=["\x74\x72\x61\x6E\x73\x66\x6F\x72\x6D","\x73\x74\x79\x6C\x65","\x2E\x62\x67\x58","\x71\x75\x65\x72\x79\x53\x65\x6C\x65\x63\x74\x6F\x72","\x74\x72\x61\x6E\x73\x6C\x61\x74\x65\x58\x28","\x70\x78\x29\x20\x74\x72\x61\x6E\x73\x6C\x61\x74\x65\x59\x28","\x70\x78\x29","\x2E\x6F\x62\x6A\x65\x63\x74\x2D\x70\x61\x72\x61\x6C\x6C\x61\x78","\x2E\x6F\x62\x6A\x65\x63\x74\x2D\x70\x61\x72\x61\x6C\x6C\x61\x78\x32","\x62\x6F\x74\x74\x6F\x6D","\x31\x30\x76\x68","\x74\x72\x61\x6E\x73\x69\x74\x69\x6F\x6E","\x62\x6F\x74\x74\x6F\x6D\x20\x31\x2E\x30\x73\x20\x63\x75\x62\x69\x63\x2D\x62\x65\x7A\x69\x65\x72\x28\x2E\x31\x37\x2C\x2E\x36\x37\x2C\x2E\x34\x36\x2C\x2E\x39\x39\x29","\x2D\x31\x30\x30\x76\x68","\x62\x6F\x74\x74\x6F\x6D\x20\x31\x2E\x30\x73\x20\x63\x75\x62\x69\x63\x2D\x62\x65\x7A\x69\x65\x72\x28\x2E\x35\x39\x2C\x31\x2E\x33\x2C\x2E\x39\x37\x2C\x31\x2E\x30\x32\x29","\x74\x6F\x70","\x30\x76\x68","\x74\x6F\x70\x20\x31\x2E\x30\x73\x20\x63\x75\x62\x69\x63\x2D\x62\x65\x7A\x69\x65\x72\x28\x2E\x31\x37\x2C\x2E\x36\x37\x2C\x2E\x34\x36\x2C\x2E\x39\x39\x29","\x74\x6F\x70\x20\x31\x2E\x30\x73\x20\x63\x75\x62\x69\x63\x2D\x62\x65\x7A\x69\x65\x72\x28\x2E\x35\x39\x2C\x31\x2E\x33\x2C\x2E\x39\x37\x2C\x31\x2E\x30\x32\x29","\x76\x69\x73\x69\x62\x69\x6C\x69\x74\x79","\x2E\x64\x69\x61\x6C\x6F\x67","\x76\x69\x73\x69\x62\x6C\x65","\x69\x6E\x6E\x65\x72\x48\x54\x4D\x4C","\x20\x3C\x62\x72\x20\x2F\x3E","\x68\x69\x64\x64\x65\x6E","\x3C\x62\x72\x2F\x3E\x68\x69\x21\x20\x74\x6F\x75\x63\x68\x20\x6F\x72\x3C\x62\x72\x20\x2F\x3E\x63\x6C\x69\x63\x6B\x20\x74\x6F\x20\x2D\x3E","\x62\x61\x63\x6B\x67\x72\x6F\x75\x6E\x64\x50\x6F\x73\x69\x74\x69\x6F\x6E","\x23\x6B\x72\x61\x6D\x65\x72","\x70\x78","\x61\x6E\x69\x6D\x61\x74\x69\x6F\x6E","\x6E\x6F\x6E\x65","\x61\x6E\x69\x6D\x61\x74\x65\x2D\x63\x68\x61\x72\x20\x31\x2E\x30\x73\x20\x73\x74\x65\x70\x73\x28\x34\x29\x20\x69\x6E\x66\x69\x6E\x69\x74\x65","\x61\x6E\x69\x6D\x61\x74\x65\x2D\x63\x68\x61\x72\x33\x20\x31\x2E\x30\x73\x20\x73\x74\x65\x70\x73\x28\x34\x29\x20\x69\x6E\x66\x69\x6E\x69\x74\x65","\x61\x6E\x69\x6D\x61\x74\x65\x2D\x63\x68\x61\x72\x35\x20\x31\x2E\x30\x73\x20\x73\x74\x65\x70\x73\x28\x34\x29\x20\x69\x6E\x66\x69\x6E\x69\x74\x65","\x73\x65\x74\x49\x6E\x74\x65\x72\x76\x61\x6C","\x49\x20\x77\x61\x73\x20\x42\x6F\x72\x6E\x20\x6F\x6E\x3C\x62\x72\x2F\x3E\x4A\x75\x6C\x79\x20\x35\x2C\x20\x31\x39\x39\x37","\x49\x20\x77\x65\x6E\x74\x3C\x62\x72\x2F\x3E\x74\x6F\x20\x73\x63\x68\x6F\x6F\x6C","\x54\x68\x65\x6E\x20\x63\x6F\x6C\x6C\x65\x67\x65\x3C\x62\x72\x2F\x3E\x68\x61\x70\x70\x65\x6E\x65\x64","\x49\x20\x73\x68\x69\x66\x74\x65\x64\x3C\x62\x72\x2F\x3E\x63\x6F\x75\x72\x73\x65\x73","\x6D\x65\x63\x68\x2E\x20\x65\x6E\x67\x2E\x3C\x62\x72\x2F\x3E\x69\x73\x20\x68\x61\x72\x64","\x3C\x62\x72\x2F\x3E\x49\x20\x6B\x6E\x6F\x77\x20\x73\x74\x75\x66\x66","\x73\x6F\x6D\x65\x20\x6D\x6F\x72\x65\x20\x73\x74\x75\x66\x66","\x61\x6E\x64\x20\x77\x69\x6C\x6C\x69\x6E\x67\x20\x74\x6F\x3C\x62\x72\x2F\x3E\x6C\x65\x61\x72\x6E\x20\x6D\x6F\x72\x65\x20\x3A\x44","\x2E\x68\x6F\x73\x70\x69\x74\x61\x6C","\x2E\x67\x72\x61\x64\x65\x2D\x73\x63\x68\x6F\x6F\x6C","\x2E\x68\x69\x67\x68\x2D\x73\x63\x68\x6F\x6F\x6C","\x2E\x73\x6B\x69\x6C\x6C\x2D\x73\x65\x74","\x2E\x68\x74\x6D\x6C","\x2E\x63\x73\x73","\x2E\x6A\x61\x76\x61\x73\x63\x72\x69\x70\x74","\x2E\x6F\x74\x68\x65\x72\x73\x6B\x69\x6C\x6C\x2D\x73\x65\x74","\x2E\x63\x70\x70","\x2E\x6D\x79\x73\x71\x6C","\x2E\x70\x68\x70","\x2E\x72\x6F\x74\x61\x74\x6F\x72","\x74\x69\x74\x6C\x65","\x6C\x6F\x61\x64","\x61\x64\x64\x45\x76\x65\x6E\x74\x4C\x69\x73\x74\x65\x6E\x65\x72","\x74\x6F\x75\x63\x68\x73\x74\x61\x72\x74","\x74\x6F\x75\x63\x68\x65\x6E\x64","\x73\x65\x74\x54\x69\x6D\x65\x6F\x75\x74","\x6D\x6F\x75\x73\x65\x64\x6F\x77\x6E","\x6D\x6F\x75\x73\x65\x75\x70","\x72\x6F\x74\x61\x74\x65\x28\x30\x64\x65\x67\x29","\x74\x72\x61\x6E\x73\x66\x6F\x72\x6D\x20\x31\x2E\x30\x73\x20\x63\x75\x62\x69\x63\x2D\x62\x65\x7A\x69\x65\x72\x28\x2E\x31\x37\x2C\x2E\x36\x37\x2C\x2E\x34\x36\x2C\x2E\x39\x39\x29","\x72\x6F\x74\x61\x74\x65\x28\x2D\x39\x30\x64\x65\x67\x29","\x2E\x62\x67","\x65\x76\x65\x6E\x74","\x70\x72\x65\x76\x65\x6E\x74\x44\x65\x66\x61\x75\x6C\x74","\x73\x74\x6F\x70\x50\x72\x6F\x70\x61\x67\x61\x74\x69\x6F\x6E","\x63\x61\x6E\x63\x65\x6C\x42\x75\x62\x62\x6C\x65","\x72\x65\x74\x75\x72\x6E\x56\x61\x6C\x75\x65","\x74\x6F\x75\x63\x68\x6D\x6F\x76\x65","\x74\x6F\x75\x63\x68\x63\x61\x6E\x63\x65\x6C"];var _0xfa52=[_0x6e21[0],_0x6e21[1],_0x6e21[2],_0x6e21[3],_0x6e21[4],_0x6e21[5],_0x6e21[6],_0x6e21[7],_0x6e21[8],_0x6e21[9],_0x6e21[10],_0x6e21[11],_0x6e21[12],_0x6e21[13],_0x6e21[14],_0x6e21[15],_0x6e21[16],_0x6e21[17],_0x6e21[18],_0x6e21[19],_0x6e21[20],_0x6e21[21],_0x6e21[22],_0x6e21[23],_0x6e21[24],_0x6e21[25],_0x6e21[26],_0x6e21[27],_0x6e21[28],_0x6e21[29],_0x6e21[30],_0x6e21[31],_0x6e21[32],_0x6e21[33],_0x6e21[34],_0x6e21[35],_0x6e21[36],_0x6e21[37],_0x6e21[38],_0x6e21[39],_0x6e21[40],_0x6e21[41],_0x6e21[42],_0x6e21[43],_0x6e21[44],_0x6e21[45],_0x6e21[46],_0x6e21[47],_0x6e21[48],_0x6e21[49],_0x6e21[50],_0x6e21[51],_0x6e21[52],_0x6e21[53],_0x6e21[54],_0x6e21[55],_0x6e21[56],_0x6e21[57],_0x6e21[58],_0x6e21[59],_0x6e21[60],_0x6e21[61],_0x6e21[62],_0x6e21[63],_0x6e21[64],_0x6e21[65]];var xCtrSun=0;var yCtrSun=0;var xCtrMoon=0;var yCtrMoon=0;var object;var objectPopAt;var charSwitchVal=-2250;var boolGo=false;var bgX=0;var objects_parallax=0;var objects_parallax2=0;var bgY=0;function forward(){document[_0x6e21[3]](_0x6e21[66])[_0x6e21[1]][_0x6e21[0]]= _0x6e21[4]+ bgX+ _0x6e21[5]+ bgY+ _0x6e21[6];bgY-= 1;bgX-= 5;document[_0x6e21[3]](_0x6e21[7])[_0x6e21[1]][_0x6e21[0]]= _0x6e21[4]+ objects_parallax+ _0x6e21[6];document[_0x6e21[3]](_0x6e21[8])[_0x6e21[1]][_0x6e21[0]]= _0x6e21[4]+ objects_parallax2+ _0x6e21[6];objects_parallax-= 10;objects_parallax2-= 2}function objectUp(_0xa03cx10,_0xa03cx11){if(objects_parallax<= _0xa03cx11&& objects_parallax>= _0xa03cx11- 1200){document[_0x6e21[3]](_0xa03cx10)[_0x6e21[1]][_0x6e21[9]]= _0x6e21[10];document[_0x6e21[3]](_0xa03cx10)[_0x6e21[1]][_0x6e21[11]]= _0x6e21[12]}else {document[_0x6e21[3]](_0xa03cx10)[_0x6e21[1]][_0x6e21[9]]= _0x6e21[13];document[_0x6e21[3]](_0xa03cx10)[_0x6e21[1]][_0x6e21[11]]= _0x6e21[14]}}function objectDown(_0xa03cx10,_0xa03cx11){if(objects_parallax<= _0xa03cx11&& objects_parallax>= _0xa03cx11- 1200){document[_0x6e21[3]](_0xa03cx10)[_0x6e21[1]][_0x6e21[15]]= _0x6e21[16];document[_0x6e21[3]](_0xa03cx10)[_0x6e21[1]][_0x6e21[11]]= _0x6e21[17]}else {document[_0x6e21[3]](_0xa03cx10)[_0x6e21[1]][_0x6e21[15]]= _0x6e21[13];document[_0x6e21[3]](_0xa03cx10)[_0x6e21[1]][_0x6e21[11]]= _0x6e21[18]}}function showDialog(){if(bgX>  -7100){document[_0x6e21[3]](_0x6e21[20])[_0x6e21[1]][_0x6e21[19]]= _0x6e21[21]}}function changeDialog(_0xa03cx15,_0xa03cx11){if(objects_parallax<= _0xa03cx11&& objects_parallax>= _0xa03cx11- 1E3){document[_0x6e21[3]](_0x6e21[20])[_0x6e21[22]]= _0x6e21[23]+ _0xa03cx15;showDialog()}}function hideDialog(){document[_0x6e21[3]](_0x6e21[20])[_0x6e21[1]][_0x6e21[19]]= _0x6e21[24]}function initialDialog(){document[_0x6e21[3]](_0x6e21[20])[_0x6e21[22]]= _0x6e21[25]}function hideInitialDialog(){if(bgX==  -10){hideDialog()}}function kramerStatic(){document[_0x6e21[3]](_0x6e21[27])[_0x6e21[1]][_0x6e21[26]]= -600+ charSwitchVal + _0x6e21[28];document[_0x6e21[3]](_0x6e21[27])[_0x6e21[1]][_0x6e21[29]]= _0x6e21[30]}function switchChar(_0xa03cx11,_0xa03cx1b,_0xa03cx1c){if(objects_parallax<= _0xa03cx11){charSwitchVal=  -1500};if(objects_parallax<= _0xa03cx1b){charSwitchVal=  -750};if(objects_parallax<= _0xa03cx1c){charSwitchVal= 0};if(charSwitchVal== 0){document[_0x6e21[3]](_0x6e21[27])[_0x6e21[1]][_0x6e21[29]]= _0x6e21[31]};if(charSwitchVal==  -750){document[_0x6e21[3]](_0x6e21[27])[_0x6e21[1]][_0x6e21[29]]= _0x6e21[32]};if(charSwitchVal==  -1500){document[_0x6e21[3]](_0x6e21[27])[_0x6e21[1]][_0x6e21[29]]= _0x6e21[33]};if(charSwitchVal==  -2250){document[_0x6e21[3]](_0x6e21[27])[_0x6e21[1]][_0x6e21[29]]= _0x6e21[30]}}function absorbEvent(_0xa03cx1e){var _0xa03cx1f=_0xa03cx1e|| window[_0x6e21[67]];_0xa03cx1f[_0x6e21[68]]&& _0xa03cx1f[_0x6e21[68]]();_0xa03cx1f[_0x6e21[69]]&& _0xa03cx1f[_0x6e21[69]]();_0xa03cx1f[_0x6e21[70]]= true;_0xa03cx1f[_0x6e21[71]]= false;return false}window[_0x6e21[57]](_0x6e21[58],absorbEvent,false);window[_0x6e21[57]](_0x6e21[72],absorbEvent,false);window[_0x6e21[57]](_0x6e21[59],absorbEvent,false);window[_0x6e21[57]](_0x6e21[73],absorbEvent,false);function runTheseFunctionsOnLoad(){window[_0x6e21[34]](go,10);initialDialog();showDialog();kramerStatic();changeText()}function go(){if(boolGo== true&& objects_parallax>=  -19850){forward();hideInitialDialog();switchChar(-3640,-5E3,-7500);changeDialog(_0x6e21[35],-3640);changeDialog(_0x6e21[36],-5E3);changeDialog(_0x6e21[37],-7500);changeDialog(_0x6e21[38],-8250);changeDialog(_0x6e21[39],-9E3);changeDialog(_0x6e21[40],-1E4);changeDialog(_0x6e21[41],-14500);changeDialog(_0x6e21[42],-18500);objectUp(_0x6e21[43],-2500);objectDown(_0x6e21[44],-5E3);objectUp(_0x6e21[45],-7500);objectDown(_0x6e21[46],-1E4);objectDown(_0x6e21[47],-10750);objectDown(_0x6e21[48],-11750);objectDown(_0x6e21[49],-12750);objectDown(_0x6e21[50],-14250);objectUp(_0x6e21[51],-15250);objectUp(_0x6e21[52],-16250);objectUp(_0x6e21[53],-17250);objectUp(_0x6e21[54],-19600);document[_0x6e21[3]](_0x6e21[55])[_0x6e21[22]]= objects_parallax}}function auto(){if(boolGo== true){boolGo= false}else {boolGo= true}}window[_0x6e21[57]](_0x6e21[56],runTheseFunctionsOnLoad,false);window[_0x6e21[57]](_0x6e21[58],auto,false);window[_0x6e21[57]](_0x6e21[59],function(){auto();kramerStatic();window[_0x6e21[60]](hideDialog,2E3)},false);window[_0x6e21[57]](_0x6e21[61],auto,false);window[_0x6e21[57]](_0x6e21[62],function(){auto();kramerStatic();window[_0x6e21[60]](hideDialog,2E3)},false);function objectRotate(_0xa03cx10,_0xa03cx11){if(objects_parallax<= _0xa03cx11&& objects_parallax>= _0xa03cx11- 900){document[_0x6e21[3]](_0xa03cx10)[_0x6e21[1]][_0x6e21[0]]= _0x6e21[63];document[_0x6e21[3]](_0xa03cx10)[_0x6e21[1]][_0x6e21[11]]= _0x6e21[64]}else {if(objects_parallax< _0xa03cx11){document[_0x6e21[3]](_0xa03cx10)[_0x6e21[1]][_0x6e21[0]]= _0x6e21[65];document[_0x6e21[3]](_0xa03cx10)[_0x6e21[1]][_0x6e21[11]]= _0x6e21[64]}}}
+/** @type {number} */
+var xCtrSun = 0;
+/** @type {number} */
+var yCtrSun = 0;
+/** @type {number} */
+var xCtrMoon = 0;
+/** @type {number} */
+var yCtrMoon = 0;
+var object;
+var objectPopAt;
+/** @type {number} */
+var charSwitchVal = -2250;
+/** @type {boolean} */
+var boolGo = false;
+/** @type {number} */
+var bgX = 0;
+/** @type {number} */
+var objects_parallax = 0;
+/** @type {number} */
+var objects_parallax2 = 0;
+/** @type {number} */
+var bgY = 0;
+/**
+ * @return {undefined}
+ */
+function forward() {
+	bgY -= 1;
+	bgX -= 5;
+	objects_parallax -= 10;
+	objects_parallax2 -= 2;
+	document.querySelector(".bg").style.transform = "translateX(" + bgX + "px) translateY(" + bgY + "px)";
+	document.querySelector(".object-parallax").style.transform = "translateX(" + objects_parallax + "px)";
+	document.querySelector(".object-parallax2").style.transform = "translateX(" + objects_parallax2 + "px)";
+}
+/**
+ * @param {?} startEvent
+ * @param {number} opt_attributes
+ * @return {undefined}
+ */
+function objectUp(startEvent, opt_attributes) {
+  if (objects_parallax <= opt_attributes && objects_parallax >= opt_attributes - 1200) {
+    document.querySelector(startEvent).style.bottom = "10vh";
+    document.querySelector(startEvent).style.transition = "bottom 1.0s cubic-bezier(.17,.67,.46,.99)";
+  } else {
+    document.querySelector(startEvent).style.bottom = "-100vh";
+    document.querySelector(startEvent).style.transition = "bottom 1.0s cubic-bezier(.59,1.3,.97,1.02)";
+  }
+}
+/**
+ * @param {?} startEvent
+ * @param {number} opt_attributes
+ * @return {undefined}
+ */
+function objectDown(startEvent, opt_attributes) {
+  if (objects_parallax <= opt_attributes && objects_parallax >= opt_attributes - 1200) {
+    document.querySelector(startEvent).style.top = "0vh";
+    document.querySelector(startEvent).style.transition = "top 1.0s cubic-bezier(.17,.67,.46,.99)";
+  } else {
+    document.querySelector(startEvent).style.top = "-100vh";
+    document.querySelector(startEvent).style.transition = "top 1.0s cubic-bezier(.59,1.3,.97,1.02)";
+  }
+}
+/**
+ * @return {undefined}
+ */
+function showDialog() {
+  if (bgX > -7100) {
+    document.querySelector(".dialog").style.visibility = "visible";
+  }
+}
+/**
+ * @param {?} dataAndEvents
+ * @param {number} opt_attributes
+ * @return {undefined}
+ */
+function changeDialog(dataAndEvents, opt_attributes) {
+  if (objects_parallax <= opt_attributes && objects_parallax >= opt_attributes - 1E3) {
+    document.querySelector(".dialog").innerHTML = " <br />" + dataAndEvents;
+    showDialog();
+  }
+}
+/**
+ * @return {undefined}
+ */
+function hideDialog() {
+  document.querySelector(".dialog").style.visibility = "hidden";
+}
+/**
+ * @return {undefined}
+ */
+function initialDialog() {
+  document.querySelector(".dialog").innerHTML = "<br/>hi! touch or<br />click to ->";
+}
+/**
+ * @return {undefined}
+ */
+function hideInitialDialog() {
+  if (bgX == -10) {
+    hideDialog();
+  }
+}
+/**
+ * @return {undefined}
+ */
+function kramerStatic() {
+  document.querySelector("#kramer").style.backgroundPosition = -600 + charSwitchVal + "px";
+  document.querySelector("#kramer").style.animation = "none";
+}
+/**
+ * @param {number} opt_attributes
+ * @param {number} expectedNumberOfNonCommentArgs
+ * @param {number} lastArrayIdSentFromServer
+ * @return {undefined}
+ */
+function switchChar(opt_attributes, expectedNumberOfNonCommentArgs, lastArrayIdSentFromServer) {
+  if (objects_parallax <= opt_attributes) {
+    /** @type {number} */
+    charSwitchVal = -1500;
+  }
+  if (objects_parallax <= expectedNumberOfNonCommentArgs) {
+    /** @type {number} */
+    charSwitchVal = -750;
+  }
+  if (objects_parallax <= lastArrayIdSentFromServer) {
+    /** @type {number} */
+    charSwitchVal = 0;
+  }
+  if (charSwitchVal == 0) {
+    document.querySelector("#kramer").style.animation = "animate-char 1.0s steps(4) infinite";
+  }
+  if (charSwitchVal == -750) {
+    document.querySelector("#kramer").style.animation = "animate-char3 1.0s steps(4) infinite";
+  }
+  if (charSwitchVal == -1500) {
+    document.querySelector("#kramer").style.animation = "animate-char5 1.0s steps(4) infinite";
+  }
+  if (charSwitchVal == -2250) {
+    document.querySelector("#kramer").style.animation = "none";
+  }
+}
+/**
+ * @return {undefined}
+ */
+function absorbEvent(event) {
+  var e = event || window.event;
+  e.preventDefault && e.preventDefault();
+  e.stopPropagation && e.stopPropagation();
+  e.cancelBubble = true;
+  e.returnValue = false;
+  return false;
+}
+
+window.addEventListener("touchstart", absorbEvent, false);
+window.addEventListener("touchmove", absorbEvent, false);
+window.addEventListener("touchend", absorbEvent, false);
+window.addEventListener("touchcancel", absorbEvent, false);
+
+function runTheseFunctionsOnLoad() {
+  window.setInterval(go, 10);
+  initialDialog();
+  showDialog();
+  kramerStatic();
+  changeText();
+}
+/**
+ * @return {undefined}
+ */
+function go() {
+  if (boolGo == true && objects_parallax >= -19850) {
+	forward();
+    hideInitialDialog();
+    switchChar(-3640, -5E3, -7500);
+    changeDialog("I was Born on<br/>July 5, 1997", -3640);
+    changeDialog("I went<br/>to school", -5E3);
+    changeDialog("Then college<br/>happened", -7500);
+    changeDialog("I shifted<br/>courses", -8250);
+    changeDialog("mech. eng.<br/>is hard", -9E3);
+    changeDialog("<br/>I know stuff", -1E4);
+    changeDialog("some more stuff", -14500);
+    changeDialog("and willing to<br/>learn more :D", -18500);
+    objectUp(".hospital", -2500);
+    objectDown(".grade-school", -5E3);
+    objectUp(".high-school", -7500);
+    objectDown(".skill-set", -1E4);
+    objectDown(".html", -10750);
+    objectDown(".css", -11750);
+    objectDown(".javascript", -12750);
+    objectDown(".otherskill-set", -14250);
+    objectUp(".cpp", -15250);
+    objectUp(".mysql", -16250);
+    objectUp(".php", -17250);
+    objectUp(".rotator", -19600);
+    document.querySelector("title").innerHTML = objects_parallax;
+  }
+}
+/**
+ * @return {undefined}
+ */
+function auto() {
+  if (boolGo == true) {
+    /** @type {boolean} */
+    boolGo = false;
+  } else {
+    /** @type {boolean} */
+    boolGo = true;
+  }
+}
+window.addEventListener("load", runTheseFunctionsOnLoad, false);
+window.addEventListener("touchstart", auto, false);
+window.addEventListener("touchend", function() {
+  auto();
+  kramerStatic();
+  window.setTimeout(hideDialog, 2E3);
+}, false);
+window.addEventListener("mousedown", auto, false);
+window.addEventListener("mouseup", function() {
+  auto();
+  kramerStatic();
+  window.setTimeout(hideDialog, 2E3);
+}, false);
+
+
+window.addEventListener("keydown", auto, false);
+
+/**
+ * @param {?} startEvent
+ * @param {number} opt_attributes
+ * @return {undefined}
+ */
+function objectRotate(startEvent, opt_attributes) {
+  if (objects_parallax <= opt_attributes && objects_parallax >= opt_attributes - 900) {
+    document.querySelector(startEvent).style.transform = "rotate(0deg)";
+    document.querySelector(startEvent).style.transition = "transform 1.0s cubic-bezier(.17,.67,.46,.99)";
+  } else {
+    if (objects_parallax < opt_attributes) {
+      document.querySelector(startEvent).style.transform = "rotate(-90deg)";
+      document.querySelector(startEvent).style.transition = "transform 1.0s cubic-bezier(.17,.67,.46,.99)";
+    }
+  }
+}
+;
