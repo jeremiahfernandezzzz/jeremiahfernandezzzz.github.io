8c7496c63552b9ded8972be810136e9b09d5c58a
jeremiahfernandezzzz <jeremiahfernandezzzz@gmail.com>
2017-04-21 20:03:57 -0700

    Update mainobf.js

--- script/mainobf.js
+++ script/mainobf.js
@@ -215,7 +215,7 @@ function runTheseFunctionsOnLoad() {
  * @return {undefined}
  */
 function go() {
-  if (boolGo == true && objects_parallax >= -19850) {
+  if (boolGo == true && objects_parallax >= -19340) {
 	forward();
     hideInitialDialog();
     switchChar(-3640, -5E3, -7500);
